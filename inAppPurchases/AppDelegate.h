016091e6ec16de6742aa956bf969632522cbd59b
Lagspoon <lagspoon@gmail.com>
2014-10-03 14:32:09 +0200

    Initial Commit

---
+++ inAppPurchases/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  inAppPurchases
+//
+//  Created by Olivier Delecueillerie on 03/10/2014.
+//  Copyright (c) 2014 lagspoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
